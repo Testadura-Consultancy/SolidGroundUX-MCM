bfb0c4cf5faa0b6116a9d459dc852da087cf717a
Mark Fieten <mark.fieten@testadura.nl>
2026-08-29 02:09:13 +0000

    Initial project scaffold

---
+++ target-root/usr/local/lib/solidgroundux/globals/solidgrond-management-modules-definitions.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# =====================================================================================
+# SolidGrond Management Modules - Project Definitions
+# -------------------------------------------------------------------------------------
+# Metadata:
+#   Version     : 1.0
+#   Build       : 2624102
+#   Checksum    : -
+#   Source      : solidgrond-management-modules-definitions.sh
+#   Type        : library
+#   Group       : Globals
+#   Purpose     : Project-wide identity and release globals
+# =====================================================================================
+SGND_SOLIDGROND_MANAGEMENT_MODULES_PRODUCT=SolidGrond\ Management\ Modules
+SGND_SOLIDGROND_MANAGEMENT_MODULES_VERSION=1.0
+SGND_SOLIDGROND_MANAGEMENT_MODULES_BUILD=2624102
